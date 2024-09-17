dc7e70923afac0798b3d003f17243740e4e39d39
Daniel <danielmartins10@gmail.com>
2024-09-17 15:07:28 -0300

    refactor: run `npm run prettier fix`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,30 +9,34 @@ describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response.status).toBe(201);
-      
+
         const responseBody = await response.json();
-      
+
         expect(Array.isArray(responseBody)).toBe(true);
         expect(responseBody.length).toBeGreaterThan(0);
       });
 
       test("For the second time", async () => {
-        const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response1.status).toBe(200);
-      
+
         const responseBody1 = await response1.json();
-      
+
         expect(Array.isArray(responseBody1)).toBe(true);
         expect(responseBody1.length).toBe(0);
       });
     });
   });
 });
-
-
