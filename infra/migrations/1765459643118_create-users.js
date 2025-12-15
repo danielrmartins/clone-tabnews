14b6a68f550af1a91074cd2cee98623c0d9a4a7a
Daniel <danielmartins10@gmail.com>
2025-12-15 14:17:00 -0300

    refactor: `user` migration

--- infra/migrations/1765459643118_create-users.js
+++ infra/migrations/1765459643118_create-users.js
@@ -20,21 +20,23 @@ exports.up = (pgm) => {
       unique: true,
     },
 
-    // Why 78 in length? https://security.stackexchange.com/a/39851/379542
+    // Why 60 in length? See https://www.npmjs.com/package/bcrypt#hash-info
     password: {
-      type: "varchar(78)",
+      type: "varchar(60)",
       notNull: true,
     },
 
     // Why timestamp with time zone? See https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
 
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
