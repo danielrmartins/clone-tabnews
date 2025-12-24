b715311fdf07316d42b3ac117b829cb65a9acd48
Daniel <danielmartins10@gmail.com>
2025-12-24 13:41:54 -0300

    feat: implement session deletion

--- models/session.js
+++ models/session.js
@@ -86,11 +86,36 @@ async function renew(sessionId) {
   }
 }
 
+async function expiredById(sessionId) {
+  const expiredSessionObject = await runUpdateQuery(sessionId);
+
+  return expiredSessionObject;
+
+  async function runUpdateQuery(sessionId) {
+    const results = await database.query({
+      text: `
+      UPDATE 
+        sessions 
+      SET 
+        expires_at = expires_at - INTERVAL '1 year',
+        updated_at = NOW() 
+      WHERE 
+        id = $1
+      RETURNING *
+      ;`,
+      values: [sessionId],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
   findOneValidByToken,
-  EXPIRATION_IN_MILLISECONDS,
   renew,
+  expiredById,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default session;
